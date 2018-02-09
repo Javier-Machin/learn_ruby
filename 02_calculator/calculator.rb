d15d5b0d142f7a8e0bd4b80e269a715a19394954
Javier Machin <javier_machin_acosta@hotmail.com>
2018-02-09 11:54:03 +0000

    Add solution for the 3 first exercises

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,35 @@
-#write your code here
+def add(number1, number2)
+	number1 + number2
+end
+
+def subtract(number1, number2)
+	number1 - number2
+end
+
+def sum(array)
+	total = 0
+	array.each { |n| total += n }
+	total
+end
+
+def multiply(*numbers)
+	total = 1
+	numbers.each { |number| total*= number }
+	total
+end
+
+def power(number1, number2)
+	number1 ** number2
+end
+
+def factorial(number)
+	return 1 if number == 1 || number == 0
+	total = number
+	i = 1
+	(number - 1).times do |n|
+		total *= number - i
+		i += 1
+	end
+	total
+end
+
