6a5a0dddad8be8ed552e999f02db68195ff10acd
Javier Machin <javier_machin_acosta@hotmail.com>
2018-02-10 11:36:27 +0000

    Add solution for book_titles and timer exercises

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,28 @@
 class Book
-# write your code here
+	IGNORED_WORDS = ["on", "in", "at", "since", "for", "ago",
+					 "before", "to", "past", "and" ,"of", "the" ,"a", "an"]
+
+	attr_reader :title
+
+	def title=(string)
+		@title = titleize(string)
+	end
+
+	def titleize(string)
+		i = 0
+		titleized_string = ""
+		string = string.split(" ")
+		return string[0].capitalize if string.length == 1
+		string.each do |str|
+			if i == 0 || str.length >= 4 || i == string.length - 1 || !(IGNORED_WORDS.include?(str))
+				titleized_string += str.capitalize + " "
+			else
+				titleized_string += str + " "
+			end
+			i += 1
+		end
+		titleized_string.strip
+	end
+	
 end
+
--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,35 @@
 class Timer
-  #write your code here
+	attr_reader :seconds
+
+	def initialize
+		@seconds = 0
+		@minutes = 0
+		@hours = 0
+	end
+
+	def seconds=(num)
+		@seconds = num
+	end
+
+	def time_string
+		if @seconds > 60
+			@minutes = @seconds / 60
+			@seconds = @seconds % 60
+			if @minutes > 60
+				@hours = @minutes / 60
+				@minutes = @minutes % 60
+			end
+		end
+
+		seconds_string = @seconds.to_s
+		minutes_string = @minutes.to_s
+		hours_string = @hours.to_s
+
+		seconds_string = "0" + seconds_string if seconds_string.length == 1
+		minutes_string = "0" + minutes_string if minutes_string.length == 1
+		hours_string = "0" + hours_string if hours_string.length == 1
+
+		return hours_string + ":" + minutes_string + ":" + seconds_string
+	end
+
 end
